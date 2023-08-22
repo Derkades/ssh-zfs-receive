dba5f5485509c5d0f58469ad2475cd2cef6eaecf
Thomas Backman <serenity@exscape.org>
2023-08-22 15:41:13 +0200

    Updates to work with Syncoid 2.1.0
    1) Add SYNCOID_SNAPSHOT regex, and allow zfs destroy on matching snapshots.
    Syncoid needs to create and destroy sync snapshots for operation without --no-sync-snap to work.
    
    2) Split commands on semicolons and verify them one by one.
    Syncoid sends multiple (up to 10) zfs destroy commands at once when pruning snapshots.
    
    3) Allow another variation on the mbuffer ... | zfs receive commands
    with and without -F

--- restrict_zfs.py
+++ restrict_zfs.py
@@ -2,11 +2,15 @@ import os
 import re
 import syslog
 import sys
+import subprocess
 
 POOL = r"'[A-Za-z0-9_-]+'"
 DATASET = r"'[A-Za-z0-9_\/-]+'"
 DATASET_SNAPSHOT = r"'[A-Za-z0-9_\/-]+'@'[A-Za-z0-9:_-]+'"
 
+# Used when syncoid calls zfs destroy -- ONLY snapshots that match this regex will be allowed to be destroyed.
+# Note: Syncoid does NOT single quote the snapshot name as of version 2.1.0, but let's allow that.
+SYNCOID_SNAPSHOT = r"'[A-Za-z0-9_\/-]+'@('?)syncoid_[A-Za-z0-9:_-]+\1"
 
 # These commands were issued by Syncoid with standard options. If in your
 # usage you notice any commands that should be allowed but aren't allowed
@@ -20,11 +24,14 @@ ALLOWED_COMMANDS = [
     r'zfs get -H (name|receive_resume_token) ' + DATASET,
     r'zfs get -Hpd 1 -t snapshot guid,creation ' + DATASET,
     r'zfs get -H -p used ' + DATASET,
-    r' lzop -dfc \|  zfs receive  -s -F ' + DATASET,
-    r' lzop -dfc \|  zfs receive  -s -F ' + DATASET + r' 2>&1',
-    r' zfs rollback -R ' + DATASET_SNAPSHOT,
-    r' mbuffer (-r \d+[kM])? -q -s \d+[kM] -m \d+[kM] 2>\/dev\/null \| lzop -dfc \|  zfs receive  -s -F ' + DATASET,
-    r' mbuffer (-r \d+[kM])? -q -s \d+[kM] -m \d+[kM] 2>\/dev\/null \| lzop -dfc \|  zfs receive  -s -F ' + DATASET + ' 2>&1',
+    r'lzop -dfc \|  zfs receive  -s -F ' + DATASET,
+    r'lzop -dfc \|  zfs receive  -s -F ' + DATASET + r' 2>&1',
+    # If syncoid --no-sync-snap is *not* used, the following line may work with SYNCOID_SNAPSHOT
+    # instead of DATASET_SNAPSHOT to be more restrictive
+    r'zfs rollback -R ' + DATASET_SNAPSHOT,
+    r'mbuffer (-r \d+[kM])? -q -s \d+[kM] -m \d+[kM] 2>\/dev\/null \| lzop -dfc \|  zfs receive  -s (-F)? ' + DATASET,
+    r'mbuffer (-r \d+[kM])? -q -s \d+[kM] -m \d+[kM] 2>\/dev\/null \| lzop -dfc \|  zfs receive  -s (-F)? ' + DATASET + ' 2>&1',
+    r'zfs destroy ' + SYNCOID_SNAPSHOT,
 ]
 
 COMPILED = [re.compile(command) for command in ALLOWED_COMMANDS]
@@ -39,13 +46,19 @@ def check_allowed(command: str):
 
 
 if __name__ == '__main__':
-    command = os.environ['SSH_ORIGINAL_COMMAND']
+    original_command = os.environ['SSH_ORIGINAL_COMMAND']
 
-    is_allowed = check_allowed(command)
-    if not is_allowed:
-        syslog.syslog('blocked command: ' + command)
-        sys.exit(1)
+    # Syncoid can send multiple destroy commands separated by a semicolon when pruning.
+    # Split them so that we can validate each command on its own.
+    commands = original_command.split(';')
 
-    command2 = ['sh', '-c', command]
-    syslog.syslog('running command: ' + str(command2))
-    os.execlp(command2[0], *command2)
+    for command in commands:
+        command = command.strip()
+        is_allowed = check_allowed(command)
+        if not is_allowed:
+            syslog.syslog('blocked command: ' + command)
+            sys.exit(1)
+
+        command2 = ['sh', '-c', command]
+        syslog.syslog('running command: ' + str(command2))
+        subprocess.run(command2)
